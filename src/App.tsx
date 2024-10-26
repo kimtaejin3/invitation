facb62e254683af897de5958d34827f1a964018b
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-26 19:11:34 +0900

    웨딩 데이터 가져온 후 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
@@ -6,7 +6,19 @@ import styles from './App.module.scss'
 const cx = classNames.bind(styles)
 
 function App() {
-  return <div className={cx('container')}>안녕</div>
+  const [wedding, setWedding] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:8888/wedding')
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+      })
+  }, [])
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
 
 export default App
