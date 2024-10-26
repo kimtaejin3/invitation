fa7e30588b2a5dbdce1d61cf9d3d7a573cde7826
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-26 19:22:07 +0900

    웨딩 데이터 로딩 및 에러처리

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,39 @@ const cx = classNames.bind(styles)
 
 function App() {
   const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
+
     fetch('http://localhost:8888/wedding')
       .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        }
+
         return response.json()
       })
       .then((data) => {
         setWedding(data)
       })
+      .catch((e) => {
+        console.log('에러발생:', e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error...</div>
+  }
 
   return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
 }
