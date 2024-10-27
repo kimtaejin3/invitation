a0de2f7947f60738cab83fe44ddaf179ea8af2fc
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-27 23:38:05 +0900

    video에 poster 이미지 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -8,7 +8,7 @@ const cx = classNames.bind(styles)
 function Video() {
   return (
     <Section className={cx('container')}>
-      <video autoPlay muted loop>
+      <video autoPlay muted loop poster="/assets/poster.jpg">
         <source src="/assets/main.mp4" type="video/mp4"></source>
       </video>
     </Section>
