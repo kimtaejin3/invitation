37f14ba3983f6d685e7a216a0ffa445d2704f956
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-29 20:33:22 +0900

    db.json에서 galleryImages 경로 수정

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -5,7 +5,17 @@ import styles from './ImageGallery.module.scss'
 const cx = classNames.bind(styles)
 
 function ImageGallery({ images }: { images: string[] }) {
-  return <Section title="사진첩">ImageGallery</Section>
+  return (
+    <Section title="사진첩">
+      <ul>
+        {images.map((src, idx) => (
+          <li key={idx}>
+            <img src={src} alt="사진첩 이미지" />
+          </li>
+        ))}
+      </ul>
+    </Section>
+  )
 }
 
 export default ImageGallery
