44f8f80a4958939ca4d4279c3453d021f41ab8b2
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-31 19:22:31 +0900

    ImageViewer swiper.css 커스텀

--- src/components/ImageViewer/index.tsx
+++ src/components/ImageViewer/index.tsx
@@ -6,18 +6,14 @@ import styles from './ImageViewer.module.scss'
 
 import 'swiper/css'
 
+import './swiper.css'
+
 const cx = classNames.bind(styles)
 
 function ImageViewer({ images }: { images: string[] }) {
   return (
     <div className={cx('dimmed')}>
-      <Swiper
-        style={{ height: '100%' }}
-        spaceBetween={20}
-        slidesPerView={1}
-        loop
-        initialSlide={0}
-      >
+      <Swiper spaceBetween={20} slidesPerView={1} loop initialSlide={0}>
         {images.map((src, idx) => (
           <SwiperSlide key={idx}>
             <img className={cx('img')} src={src} alt="이미지 뷰어" />
