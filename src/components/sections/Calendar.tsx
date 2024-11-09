110be5ca1918e61766334f949edac5457d4a11e4
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-09 20:11:43 +0900

    section title 컴포넌트 받을 수 있도록

--- src/components/sections/Calendar.tsx
+++ src/components/sections/Calendar.tsx
@@ -15,23 +15,35 @@ const css = `
   .rdp-month_caption, .rdp-nav {
     display: none;
   }
+
+  .rdp-day_button {
+    cursor: default;
+  }
+
+  .rdp-selected .rdp-day_button {
+    background: var(--red);
+    color: white;
+    border: 0;
+  }
 `
 
 function Calendar({ date }: { date: string }) {
   const weddingDate = parseISO(date)
 
   return (
-    <Section>
-      <div className={cx('wrap-header')}>
-        <span className={cx('txt-date')}>
-          {format(weddingDate, 'yyyy.MM.dd')}
-        </span>
-        <span className={cx('txt-time')}>
-          {format(weddingDate, 'aaa h시 eeee', { locale: ko })}
-        </span>
-      </div>
-
-      <div>
+    <Section
+      title={
+        <div className={cx('wrap-header')}>
+          <span className={cx('txt-date')}>
+            {format(weddingDate, 'yyyy.MM.dd')}
+          </span>
+          <span className={cx('txt-time')}>
+            {format(weddingDate, 'aaa h시 eeee', { locale: ko })}
+          </span>
+        </div>
+      }
+    >
+      <div className={cx('wrap-calendar')}>
         <style>{css}</style>
         <DayPicker
           locale={ko}
